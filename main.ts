93567a09af3760aada45bc40ed32d6f84b7d8f45
Mama Codet <64123495+MamaCodet@users.noreply.github.com>
2022-06-16 11:57:17 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,15 @@
+let led2 = 0
 basic.forever(function () {
-    if (input.soundLevel() > 15 || input.lightLevel() < 10) {
-        basic.setLedColor(0xffffff)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
-        basic.pause(3000)
+    basic.showNumber(input.lightLevel())
+    if (0 > 70) {
         basic.turnRgbLedOff()
-        basic.clearScreen()
     } else {
-        basic.turnRgbLedOff()
-        basic.clearScreen()
+        led2 = 255 - input.lightLevel() * 2
+        basic.setLedColor(basic.rgbw(
+        led2,
+        led2,
+        led2,
+        led2
+        ))
     }
 })
